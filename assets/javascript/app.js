c7f53996d8338d0304ccf273891945c1a4f13cc0
Kunal <patelkr07@gmail.com>
2019-04-10 19:05:35 -0500

    initializing firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+  
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAZJj8GOgv818gjAA9ojI1htDWWEs3pDco",
+    authDomain: "project-2-bb0ec.firebaseapp.com",
+    databaseURL: "https://project-2-bb0ec.firebaseio.com",
+    projectId: "project-2-bb0ec",
+    storageBucket: "project-2-bb0ec.appspot.com",
+    messagingSenderId: "724959629941"
+  };
+  firebase.initializeApp(config);
+
+  console.log(config);
